558d9445f11b1db158dca7c0ec445010d75578c7
brunospcastro <brunoscastro@ua.pt>
2022-11-11 15:18:33 +0000

    tests

--- src/test/java/com/loop/springboot/app/PriceControllerAcceptanceTest.java
+++ src/test/java/com/loop/springboot/app/PriceControllerAcceptanceTest.java
@@ -2,19 +2,17 @@ package com.loop.springboot.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import com.loop.springboot.app.model.Price;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PriceControllerAcceptanceTest {
@@ -31,13 +29,56 @@ public class PriceControllerAcceptanceTest {
         url = "http://localhost:" + randomServerPort + "/api/price/getPrice";
     }
 
+
+    //Teste 1: pedido às 10:00 do dia 14 para o produto 35455 para a marca 1 (LOJA).
     @Test
-    void shouldGetCustomWelcomeMessage() throws Exception {
-        ResponseEntity responseEntity = restTemplate.getForEntity(url + "?brand_id=1&product_id=35455&date=2020-06-14 00:00:00", List.class);
-        //assertEquals(OK, responseEntity.getStatusCode());
+    void test1() throws Exception {
+        ResponseEntity responseEntity = restTemplate.getForEntity(url + "?brand_id=1&product_id=35455&date=2020-06-14 10:00:00", List.class);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        System.out.println("Teste: "+responseEntity.getStatusCode());
         List list = (List) responseEntity.getBody();
         int id = (int) ((HashMap) list.get(0)).get("id");
         assertEquals(1, id);
+    }
+
+    //Teste 2: pedido às 16:00 do dia 14 para o produto 35455 para a marca 1 (LOJA)
+    @Test
+    void test2() throws Exception {
+        ResponseEntity responseEntity = restTemplate.getForEntity(url + "?brand_id=1&product_id=35455&date=2020-06-14 16:00:00", List.class);
+        //assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        List list = (List) responseEntity.getBody();
+        int id = (int) ((HashMap) list.get(0)).get("id");
+        assertEquals(2, id);
+    }
+    
+    //Teste 3: pedido às 21:00 do dia 14 para o produto 35455 para a marca 1 (LOJA)
+    @Test
+    void test3() throws Exception {
+        ResponseEntity responseEntity = restTemplate.getForEntity(url + "?brand_id=1&product_id=35455&date=2020-06-14 21:00:00", List.class);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        List list = (List) responseEntity.getBody();
+        int id = (int) ((HashMap) list.get(0)).get("id");
+        assertEquals(1, id);
+    }
+    
+    //Teste 4: pedido às 10:00 do dia 15 do produto 35455 para a marca 1 (LOJA)
+    @Test
+    void test4() throws Exception {
+        ResponseEntity responseEntity = restTemplate.getForEntity(url + "?brand_id=1&product_id=35455&date=2020-06-15 10:00:00", List.class);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        List list = (List) responseEntity.getBody();
+        int id = (int) ((HashMap) list.get(0)).get("id");
+        assertEquals(3, id);
+    }
+
+    //Teste 5: pedido às 21:00 do dia 16 do produto 35455 para a marca 1 (LOJA)
+    @Test
+    void test5() throws Exception {
+        ResponseEntity responseEntity = restTemplate.getForEntity(url + "?brand_id=1&product_id=35455&date=2020-06-16 21:00:00", List.class);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        List list = (List) responseEntity.getBody();
+        int id = (int) ((HashMap) list.get(0)).get("id");
+        assertEquals(4, id);
     }
     
 }
